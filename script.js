bcebb615a61a89fec692164e7f890fd4ca127971
Timilehin Amolegbe <timilehinamolegbe1@gmail.com>
2024-07-15 09:26:55 +0100

    This Keyword for regular function and arrow function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -35,3 +36,53 @@ const firstName = 'Timmy';
 calcAge(2002);
 //console.log(age);
 //printAge();
+
+
+*/
+
+/*
+//This Keyword for regular function and arrow function
+
+// var firstName = 'Micheal';
+
+const timmy = {
+  firstName: 'Timmy',
+  year: 2002,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this;
+    // const isGenZ = function () {
+    //   vonsole.log(self);
+    //   console.log(self.year >= 1998 && self.year <= 2007);
+    // };
+
+    // Solution 2
+    const isGenZ = () => {
+      vonsole.log(this);
+      console.log(this.year >= 1998 && this.year <= 2007);
+    };
+    isGenZ();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+timmy.greet();
+timmy.calcAge();
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  caonsole.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
+*/
